e91570de9d1fbc75bc434a483ca408cf35854f42
boceto1 <janka.obando@outlook.es>
2023-06-28 08:49:34 -0500

    feat: read clipData from csv files
    Use a .csv file as source of information to generate clips

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
 from moviepy.editor import *
 from os.path import exists
+from os import mkdir
 from shutil import copyfile
 from colorama import Fore
+import pandas as pd
+from re import sub
 
+def toCamelCase(string):  
+    string = sub(r"(_|-)+", " ", string).title().replace(" ", "")  
+    return string[0].lower() + string[1:]  
 
-def createClips(baseVideo, clipTimes):
-    clips = map(lambda clipTime: baseVideo.subclip(clipTime[0], clipTime[1]), clipTimes)
+def createClip(baseVideo, clipData):
+  name,startTime,endTime = clipData
+  clip = baseVideo.subclip(startTime, endTime)
+  fileName = toCamelCase(name) + '.mp4'
+  return [fileName, clip]
+
+def createClips(baseVideo, clipsData):
+    clips = map(lambda clipData: createClip(baseVideo, clipData), clipsData)
     return clips
 
-def saveClips(clips):
-  for index, clip in enumerate(clips):
-    clip.write_videofile(f"test-clip{index}.mp4")
+def saveClips(clipsInfo):
+  mkdir('./out')
+  for index, clipInfo in enumerate(clipsInfo):
+    fileName, clip = clipInfo
+    print(Fore.BLUE + 'Creating ' + f"{index +1}_{fileName}")
+    print(Fore.WHITE)
+    clip.write_videofile(f"./out/{index +1}_{fileName}")
+    print(Fore.GREEN + fileName + 'created successfully')
 
-def clipsFileExists():
-  if exists('./clips.csv'):
+def clipsFileExists(clipsPath='./clips.csv'):
+  if exists(clipsPath):
     return True
   print(Fore.RED + 'You are mising clips.csv file')
   print(Fore.BLUE + 'Creating clips.csv file...')
-  copyfile('./templates/clips.csv', './clips.csv')
+  copyfile('./templates/clips.csv', clipsPath)
   print(Fore.YELLOW + 'Please, fill clips.csv file and run the program again')
   return False
 
-def main():
-   if (not clipsFileExists()):
-      return
+def getClipsData(clipsPath='./clips.csv'):
+  clipsData = pd.read_csv(clipsPath)
+  return clipsData.values
 
-  #  baseVideo = VideoFileClip("test-video.mp4")
-  #  clips = createClips(baseVideo, [[831, 950]])
-  #  saveClips(clips)
+def main():
+  if (not clipsFileExists()):
+    return
+  baseVideo = VideoFileClip("test-video.mp4")
+  clipsData = getClipsData()
+  clipsInfo = createClips(baseVideo, clipsData)
+  saveClips(clipsInfo)
 
 main()
-
-
-
-
