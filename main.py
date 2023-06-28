703bfd675b3f6c739c0271ed61ada11309abba9a
boceto1 <janka.obando@outlook.es>
2023-06-28 07:55:40 -0500

    feat: check if clips.csv file exists
    clips.csv is the place where the program is going to read the times
    to create clips

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from moviepy.editor import *
+from os.path import exists
+from shutil import copyfile
+from colorama import Fore
+
 
 def createClips(baseVideo, clipTimes):
     clips = map(lambda clipTime: baseVideo.subclip(clipTime[0], clipTime[1]), clipTimes)
@@ -8,10 +12,22 @@ def saveClips(clips):
   for index, clip in enumerate(clips):
     clip.write_videofile(f"test-clip{index}.mp4")
 
+def clipsFileExists():
+  if exists('./clips.csv'):
+    return True
+  print(Fore.RED + 'You are mising clips.csv file')
+  print(Fore.BLUE + 'Creating clips.csv file...')
+  copyfile('./templates/clips.csv', './clips.csv')
+  print(Fore.YELLOW + 'Please, fill clips.csv file and run the program again')
+  return False
+
 def main():
-   baseVideo = VideoFileClip("test-video.mp4")
-   clips = createClips(baseVideo, [[831, 950]])
-   saveClips(clips)
+   if (not clipsFileExists()):
+      return
+
+  #  baseVideo = VideoFileClip("test-video.mp4")
+  #  clips = createClips(baseVideo, [[831, 950]])
+  #  saveClips(clips)
 
 main()
 
