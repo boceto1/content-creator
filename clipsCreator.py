e9d40551fa5b9af1735d0e70342fcb33867d3310
boceto1 <janka.obando@outlook.es>
2023-07-20 14:01:04 -0500

    feat: customize footer text

--- clipsCreator.py
+++ clipsCreator.py
@@ -12,20 +12,20 @@ def createClips(baseVideo, clipsData):
     clips = map(lambda clipData: createClip(baseVideo, clipData), clipsData)
     return clips
 
-def createReel(clipWithInfo, endVideo, isPreview):
+def createReel(clipWithInfo, endVideo, footerText, isPreview):
   clip, clipInfo = clipWithInfo
   topText, bottomText = clipInfo['text']
   clipDuration = MOCK_DEFAULT_DURATION if isPreview else clip.duration
 
   baseVideoShape = ColorClip(size =(REELS_DIMENSION[0], REELS_DIMENSION[1]), color =[30, 30, 30], duration=clipDuration)
 
-  footerText = getFooterText().set_duration(clipDuration)
+  styeledFooterText = getFooterText(footerText).set_duration(clipDuration)
   topTextClip = getTopTextClip(topText).set_duration(clipDuration)
   bottomTextClip = getBottomTextClip(bottomText).set_duration(clipDuration)
 
   clips = [
     baseVideoShape.set_start(0),
-    footerText.set_start(0),
+    styeledFooterText.set_start(0),
     topTextClip.set_start(0),
     bottomTextClip.set_start(0),
   ]
@@ -44,10 +44,12 @@ def createReel(clipWithInfo, endVideo, isPreview):
   
   return reel
 
-def createReels(baseVideo, clipsInfo, endVideo, isPreview):
+# reelsInfo: { baseVideo, clipsInfo, endClip, footerText }
+def createReels(reelsInfo, isPreview):
+  baseVideo,clipsInfo, endClip, footerText = reelsInfo['baseVideo'], reelsInfo['clipsInfo'], reelsInfo['endClip'], reelsInfo['footerText']
   clips = createClips(baseVideo, clipsInfo)
   clipsWithInfo = zip(clips, clipsInfo)
-  reels = map(lambda clipWithInfo: createReel(clipWithInfo, endVideo, isPreview), clipsWithInfo)
+  reels = map(lambda clipWithInfo: createReel(clipWithInfo, endClip, footerText, isPreview), clipsWithInfo)
   return reels
 
 def saveClips(projectName, clipsInfo, reels):
@@ -65,5 +67,12 @@ def generateReels(projectData, isPreview):
   baseVideo = VideoFileClip(projectData['baseVideo'])
   endClip = VideoFileClip(projectData['endClip'])
   resizedVideo = baseVideo.resize((VIDEO_DIMENSION[0], VIDEO_DIMENSION[1]))
-  reels = createReels(resizedVideo, projectData['clips'], endClip, isPreview)
+
+  reelsInfo = {
+    'baseVideo': resizedVideo,
+    'clipsInfo': projectData['clips'],
+    'endClip': endClip,
+    'footerText': projectData['footerText'],
+  }
+  reels = createReels(reelsInfo, isPreview)
   saveClips(projectData['projectName'], projectData['clips'], reels)
